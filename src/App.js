de7857f62e6be4cd07b662fe36c3fdcc13a1463a
Lívia Cardoso <livia.cardoso@superior-ind.com>
2022-10-21 16:45:34 -0300

    Part 3

--- src/App.js
+++ src/App.js
@@ -3,16 +3,51 @@ import {NavigationContainer} from '@react-navigation/native';
 import {createStackNavigator} from '@react-navigation/stack';
 import UserList from './views/UserList';
 import UserForm from './views/UserForm';
+import {Button} from 'react-native';
+import {Icon} from 'react-native-vector-icons/MaterialIcons';
 
 const Stack = createStackNavigator();
 
 export default props => {
   return (
     <NavigationContainer>
-      <Stack.Navigator initialRouteName="UserList">
-        <Stack.Screen name="UserList" component={UserList} />
-        <Stack.Screen name="UserForm" component={UserForm} />
+      <Stack.Navigator
+        initialRouteName="UserList"
+        screenOptions={screenOptions}>
+        <Stack.Screen
+          name="UserList"
+          component={UserList}
+          options={() => {
+            return {
+              title: 'Lista de Usuários',
+              headerRight: () => (
+                <Button
+                  type="clear"
+                  icon={<Icon name="add" size={25} color="white" />}
+                />
+              ),
+            };
+          }}
+        />
+        <Stack.Screen
+          name="UserForm"
+          component={UserForm}
+          options={{
+            title: 'Formulário de Usuários',
+          }}
+        />
       </Stack.Navigator>
     </NavigationContainer>
   );
+};
+
+const screenOptions = {
+  headerStyle: {
+    backgroundColor: '#F4511E',
+  },
+
+  headerTintColor: '#FFF',
+  headerTitleStyle: {
+    fontWeight: 'bold',
+  },
 };
